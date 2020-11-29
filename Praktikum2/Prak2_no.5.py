09a2d37720aa8b100dd2797e14df52403fbb6f7b
aniszahrodl <aniszahrodl@gmail.com>
2020-11-29 23:35:26 +0700

    Chapter 07

--- Praktikum2/Prak2_no.5.py
+++ Praktikum2/Prak2_no.5.py
@@ -1,17 +1,7 @@
-# blok try-except
 try:
-    #membuka dan mau membaca file d:/data.txt
     file=open("c:/data.txt", "r")
-
-    #baca baris pertama dari file
-    # simpan ke dalam variabel bil1 sbg integrer
     bil1= int(file.readline())
-
-    #baca baris pertama dari file
-    # simpan ke dalam variabel bil2 sbg integrer
     bil2= int(file.readline())
-
-    #hitung dan tampilkan hasil bagi
     hasil = bil1/bil2
     print(bil1, 'dibagi', bil2, 'sama dengan', hasil)
 except FileNotFoundError:
